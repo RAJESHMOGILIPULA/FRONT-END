86a64fc5f5dfe805935ceb8932ab4bd423cdf7ff
RAJESHMOGILIPULA <rajeshmogilipula@gmail.com>
2024-09-23 22:42:48 +0530

    added to the path

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,7 @@ class helloWorld{
     public static void main(String[]args){
         for(int i =0;i<20 ;i++){
             System.out.println(1);
+            System.out.println("hello world!");
         }
     }
 }
